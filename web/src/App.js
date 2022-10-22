0bafc6f0ab493ce366901e07a395f40f690064f0
Sean Choi <szeanchoi@gmail.com>
2022-10-22 00:09:36 -0400

    rearrangements and css

---
+++ web/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Home from './components/Home'
+import About from './components/About'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {/* Navbar */}
+        <div className = "nav"> 
+          <span onselectstart = "return false" id = "name">szean.</span>
+
+          <span id = "linkspan">
+            <Link to = '/' className = "links">home</Link>
+            <Link to = 'about' className = "links">about</Link>
+          </span>
+        </div>
+
+        <Routes>
+          <Route path = "/" element = {<Home/>}/>
+          <Route path = "about" element = {<About/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
