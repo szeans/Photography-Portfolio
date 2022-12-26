af046914fb1cf70b43a3395ec9adfe87579ee3f5
Sean Choi <szeanchoi@gmail.com>
2022-12-25 22:56:01 -0600

    redo nav

--- web/src/App.js
+++ web/src/App.js
@@ -9,15 +9,20 @@ function App() {
       <BrowserRouter>
         {/* Navbar */}
         <div className = "nav"> 
-          <span onselectstart = "return false" id = "name">szean.</span>
-
-          <span id = "linkspan">
-            <Link to = '/' className = "links">home</Link>
-            <Link to = 'about' className = "links">about</Link>
+          <span onselectstart = "return false" id = "name">szean choi</span>
+          <span id = "pics">
+            <Link to = '/' className = "links">b&w</Link>
+            <Link to = 'about' className = "links">color</Link>
+          </span>
+          <span id = "extras">
+            <Link to = '/' className = "links">contact</Link>
+            <a href= "https://instagram.com/szeans" target="_blank" className = "links">instagram</a>
+            <a href="https://github.com/szeans" target="_blank" className = "links">github</a>
+            <a href="https://soundcloud.com/szean" target="_blank" className = "links">soundcloud</a>
           </span>
         </div>
 
-        <Routes>
+        <Routes className= "other">
           <Route path = "/" element = {<Home/>}/>
           <Route path = "about" element = {<About/>}/>
         </Routes>
