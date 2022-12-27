7927ce3306a1ba6e2862c2489fc846a94b48b7cf
Sean Choi <szeanchoi@gmail.com>
2022-12-26 22:02:28 -0600

    added archive and active link

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Archive from './components/Archive'
 import BNW from './components/BNW'
 import Color from './components/Color'
 import Contact from './components/Contact'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 
 function App() {
   return (
@@ -15,22 +16,24 @@ function App() {
           </span>
           <br/><br/>
           <span id = "pics">
-            <Link to = '/' className = "links">b&w</Link>
-            <Link to = 'color' className = "links">color</Link>
+            <NavLink to = 'bnw' className = "links">b&w</NavLink>
+            <NavLink to = 'color' className = "links">color</NavLink>
+            <NavLink to = 'archive' className = "links">archive</NavLink>
           </span>
           <br/><br/>
           <span id = "extras">
-            <Link to = 'contact' className = "linkss">contact</Link>
+            <NavLink to = 'contact' className = "linkss">contact</NavLink>
             <a href= "https://instagram.com/szeans" target="_blank" className = "linkss">instagram</a>
             <a href="https://github.com/szeans" target="_blank" className = "linkss">github</a>
             <a href="https://soundcloud.com/szean" target="_blank" className = "linkss">soundcloud</a>
-            <a href = "https://mega.nz/folder/rt0k0ZzC#10vGO_SSVE_y0JWA4Ygxwg" target="_blank" className = "linkss">archives (via MEGA)</a>
           </span>
         </div>
 
         <Routes className= "other">
           <Route path = "/" element = {<BNW/>}/>
+          <Route path = "bnw" element = {<BNW/>}/>
           <Route path = "color" element = {<Color/>}/>
+          <Route path = "archive" element = {<Archive/>}/>
           <Route path = "contact" element = {<Contact/>}/>
         </Routes>
       </BrowserRouter>
---
+++ web/src/components/Archive.js
@@ -0,0 +1,9 @@
+function Archive() {
+  return (
+    <div>
+      <h3>bunch of folders</h3>
+    </div>
+  );
+}
+
+export default Archive;
